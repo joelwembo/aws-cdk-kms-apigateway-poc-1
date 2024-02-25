092e9135a9363bd145a684d2f99c3f9713864758
Joel Wembo <joelwembo@outlook.com>
2024-02-25 17:51:16 +0800

    Initial commit

---
+++ test/kms-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KmsDemo from '../lib/kms-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kms-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KmsDemo.KmsDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
